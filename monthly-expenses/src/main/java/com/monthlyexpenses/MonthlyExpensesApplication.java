46c205226c5241ef805f5e3bdc511155f8427744
lalit.kumar <lalitraghavji1996@gmail.com>
2025-03-23 23:11:45 +0530

    hello

---
+++ monthly-expenses/src/main/java/com/monthlyexpenses/MonthlyExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.monthlyexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlyExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlyExpensesApplication.class, args);
+	}
+
+}
